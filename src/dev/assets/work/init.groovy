89806bf9fae2fdaebb1d66fe0864ed632d189ab9
Felix Simmendinger <felix.simmendinger@coremedia.com>
2016-11-10 14:24:38 +0100

    configure jenkins
    * jenkins location
    * credential for github token, expects env var GITHUB_OAUTH_TOKEN
    * github config

--- src/dev/assets/work/init.groovy
+++ src/dev/assets/work/init.groovy
@@ -1,3 +1,35 @@
-// Contains Groovy code for Jenkins initialisation
+import com.cloudbees.plugins.credentials.CredentialsScope
+import com.cloudbees.plugins.credentials.domains.Domain
+import hudson.util.Secret
+import jenkins.model.JenkinsLocationConfiguration
+import org.jenkinsci.plugins.github.GitHubPlugin
+import org.jenkinsci.plugins.github.config.GitHubPluginConfig
+import org.jenkinsci.plugins.github.config.GitHubServerConfig
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
 
-System.setProperty("test", "CoreMedia Hackathon");
+// configure JENKINS_URL
+JenkinsLocationConfiguration jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+jenkinsLocationConfiguration.adminAddress = 'test@localhost'
+jenkinsLocationConfiguration.setUrl("https://${InetAddress.localHost.hostName}")
+jenkinsLocationConfiguration.save()
+
+// configure credentials
+
+def credentialsID = 'github-oauth-token'
+def domain = Domain.global()
+def store = jenkins.model.Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+if (store.getCredentials(domain).find { it.id == credentialsID } == null) {
+  def secretText = new StringCredentialsImpl(
+          CredentialsScope.GLOBAL,
+          credentialsID,
+          "github personal access token" as String,
+          Secret.decrypt(System.getenv('GITHUB_OAUTH_TOKEN') ?: 'DUMMY')
+  )
+  store.addCredentials(domain, secretText)
+}
+
+// configure github plugin
+GitHubPluginConfig pluginConfig = GitHubPlugin.configuration()
+GitHubServerConfig serverConfig = new GitHubServerConfig('github-oauth-token')
+pluginConfig.setConfigs([serverConfig])
+pluginConfig.save()
